fc43300b0e6670df678111986c8b7e4ff7b0f413
willian <54144699+willmont1982@users.noreply.github.com>
2020-03-22 09:39:42 -0300

    Add files via upload

---
+++ server/src/Agencia.Domain/Agencia/ValueObjects/DadosBancarios.cs
@@ -0,0 +1,24 @@
+﻿using Core.Domain.ValueObjects;
+
+namespace Agencia.Domain.Agencia.ValueObjects
+{
+    public class DadosBancarios : ValueObject<DadosBancarios>
+    {
+        private DadosBancarios() { }
+
+        public string NumeroAgencia { get; private set; }
+        public string DigitoVerificador { get; private set; }
+
+        public static class Factory
+        {
+            public static DadosBancarios CriarDadosBancarios(string numeroAgencia, string digitoVerificador)
+            {
+                return new DadosBancarios
+                {
+                    NumeroAgencia = numeroAgencia,
+                    DigitoVerificador = digitoVerificador
+                };
+            }
+        }
+    }
+}
