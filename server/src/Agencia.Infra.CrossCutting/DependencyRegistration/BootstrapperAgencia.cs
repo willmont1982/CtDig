d77c63625c687f5b7d17e34fdd8ee00975256c13
willian <54144699+willmont1982@users.noreply.github.com>
2020-03-22 09:41:23 -0300

    Add files via upload

---
+++ server/src/Agencia.Infra.CrossCutting/DependencyRegistration/BootstrapperAgencia.cs
@@ -0,0 +1,14 @@
+﻿using Agencia.Domain.Agencia.Repository;
+using Agencia.Infra.Data.Mongo.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Agencia.Infra.CrossCutting.DependencyRegistration
+{
+    public static class BootstrapperAgencia
+    {
+        public static void RegistrarServicos(IServiceCollection services)
+        {
+            services.AddScoped<IAgenciaRepository, AgenciaRepository>();
+        }
+    }
+}
